7eaaa18659f1dd310a1351f2f96c06f14592b734
Eugene <eugene.pantelei@gmail.com>
2018-02-18 08:56:35 -0800

    Test commit

--- Code/ARDesigner/ViewControllers/AddModelViewController.swift
+++ Code/ARDesigner/ViewControllers/AddModelViewController.swift
@@ -19,6 +19,7 @@ class AddModelViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Женяss
     }
     
 
